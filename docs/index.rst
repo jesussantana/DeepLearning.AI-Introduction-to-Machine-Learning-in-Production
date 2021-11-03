fed42ec09dffe7ce8c8990df18717cc0a14d1e51
jesussantana <jesus@esmarketingdigital.com>
2021-11-03 08:30:09 +0100

    Project Organization

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. DeepLearning.AI Introduction to Machine Learning in Production documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+DeepLearning.AI Introduction to Machine Learning in Production documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In the first course of Machine Learning Engineering for Production Specialization, you will identify the various components and design an ML production system end-to-end: project scoping, data needs, modeling strategies, and deployment constraints and requirements; and learn how to establish a model baseline, address concept drift, and prototype the process for developing, deploying, and continuously improving a productionized ML application.',
+    author='Jesus Santana',
+    license='MIT',
+)
